678a289c927d752672e7a854d74dfdc776991124
Hemanta Deuba <hemanta.deuba1234@gmail.com>
2016-11-16 13:07:05 +0545

    Update area of triangle.c

--- area of triangle.c
+++ area of triangle.c
@@ -3,7 +3,7 @@ Student name: Hemanta Deuba
 Subject: Programming Fundamentals
 Roll no: 06
 Lab-sheet no: 02
-Program: BCS
+Program: Program to print area of a triangle, base & height are asked from users.
 Date: 16th November, 2016
 */
 #include<stdio.h>
@@ -16,7 +16,7 @@ scanf("%f",&b);
 printf("Enter height of the triangle:\n");
 scanf("%f",&h);
 area=0.5*b*h;
-printf("The area of the triangle is: %f",area);
+printf("The area of the triangle is: %f\n",area);
 getch();
 return(0);
 }
