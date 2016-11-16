ee903531e17bd61a4b438f67d7a46ab76564607b
Hemantadeuba1234 <hemanta.deuba1234@gmail.com>
2016-11-16 10:36:37 +0545

    Add files via upload
    WAP to print product of two numbers asked from user.

---
+++ product of two no.c
@@ -0,0 +1,22 @@
+/*
+Student name: Hemanta Deuba
+Subject: Programming Fundamentals
+Roll no: 06
+Lab-sheet no: 02
+Program: BCS
+Date: 16th November, 2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,product;
+printf("enter a number");
+scanf("%d",&a);
+printf("enter a number");
+scanf("%d",&b);
+product=a*b;
+printf("Sum of two numbers is: %d",product);
+getch();
+return(0);
+}
