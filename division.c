7dc1877ad67380a57339712ad0d42cb36687cb9e
Hemantadeuba1234 <hemanta.deuba1234@gmail.com>
2016-11-16 12:23:29 +0545

    Add files via upload

---
+++ division.c
@@ -0,0 +1,22 @@
+/*
+Student name: Hemanta Deuba
+Subject: Programming Fundamentals
+Roll no: 06
+Lab-sheet no: 02
+Program: BCS
+Date: 16th November, 2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+float a,b,divide;
+printf("enter a number");
+scanf("%f",&a);
+printf("enter a number");
+scanf("%f",&b);
+divide=a/b;
+printf("Division of two numbers is %f",divide);
+getch();
+return(0);
+}
