9c94dd4e225feee6b70f64652039e37b38be1512
Hemanta Deuba <hemanta.deuba1234@gmail.com>
2017-01-16 11:23:12 +0545

    Add files via upload

---
+++ reverse.c
@@ -0,0 +1,24 @@
+/*
+Student Name: Hemanta Deuba
+Roll no: 17
+Subject: Programming Fundamentals
+Program: WAP to check the given number is palindrome or not.
+Lab sheet: 19
+Date: January 16,2017
+*/
+#include<stdio.h>
+int main ()
+    {
+    int i,n,rev,reverse=0;
+            printf("enter the number:");
+            scanf("%d",&n);
+    i=n;
+            while (n>0)
+    {
+    rev=n%10;
+    reverse=reverse*10+rev;
+   n=n/10;
+    }
+    printf("reverse %d",reverse);
+return 0;
+}
