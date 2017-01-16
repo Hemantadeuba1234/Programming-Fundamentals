f149c73b9668010c55b2b81c2c136c59557153a7
Hemanta Deuba <hemanta.deuba1234@gmail.com>
2017-01-16 11:07:37 +0545

    Add files via upload

---
+++ loop6.c
@@ -0,0 +1,24 @@
+/*
+Student Name: Hemanta Deuba
+Roll no: 17
+Subject: Programming Fundamentals
+Program: WAP to display loop patterns.
+Lab sheet: 19
+Date: January 16,2017
+*/
+#include<stdio.h>
+#include<conio.h>
+    int main()
+        {
+        int i,j;
+        for(i=1;i<=2;i++)
+        {
+            for(j=1;j<=3;j++)
+            {
+            printf("%d",j);
+            }
+        printf("\n");
+        }
+        getch();
+        return 0;
+        }
